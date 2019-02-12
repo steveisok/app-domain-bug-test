372a0b6f2819afc85ea63d41a201a52467bed064
Steve Pfister <steveisok@gmail.com>
2019-02-12 09:08:17 -0500

    Sample that fails w/ mini but succeeds when ADHost is added to the gac w/ installed mono

---
+++ ADTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web;
+using System.Web.Hosting;
+using ADHost;
+
+namespace App
+{
+    public class TestApp
+    {
+        public static void Main()
+        {
+            HostingTest test;
+            Console.WriteLine("Starting... in " + AppDomain.CurrentDomain.Id.ToString() + " at: " + AppDomain.CurrentDomain.BaseDirectory);
+
+            ApplicationManager appMan = ApplicationManager.GetApplicationManager();
+
+            test = appMan.CreateObject("TestDomain", typeof(HostingTest), "/", @"/Users/steveman/Projects/web-folder/", true) as HostingTest;
+
+            Console.WriteLine("Before method call in " + AppDomain.CurrentDomain.Id.ToString() + " at: " + AppDomain.CurrentDomain.BaseDirectory);
+            Console.WriteLine("\n\n\n");
+
+            test.RunTest();
+
+            Console.WriteLine("\n\nDone");
+        }
+    }
+}
---
+++ MyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+
+namespace ADHost
+{
+    public class HostingTest : MarshalByRefObject, IRegisteredObject
+    {
+        public void RunTest()
+        {
+            Console.WriteLine("Hello from " + AppDomain.CurrentDomain.Id.ToString() + " at: " + AppDomain.CurrentDomain.BaseDirectory);
+        }
+
+        public void Stop(bool immediate)
+        {
+            Console.WriteLine("Stopping");
+        }
+    }
+}
